42164ee7e42039441b7bb73ad17c30006e03dbd0
Mike <mikeglopez.91@gmail.com>
2020-05-21 21:19:04 -0500

    Add native modules to non expo app

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+const App: () => React$Node = () => {
+  const pickImage = () => {
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+
+    /**
+     * The first arg is the options object for customization (it can also be null or omitted for default options),
+     * The second arg is the callback which sends object: response (more info in the API Reference)
+     */
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        console.log(response.uri);
+      }
+    });
+  }
+
+  return (
+    <View>
+      <Button title='Take Image' onPress={pickImage} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default App;
